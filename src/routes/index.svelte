b289ffdf7033bf149cf4fb900b797a4b76f403ff
IsaacMorrier <isaacmorrier@gmail.com>
2020-12-19 11:42:02 -0500

    add scss grid

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -14,11 +14,16 @@
 <PageTransition>
 
 	<h1>Home</h1>
-
-	<Image src="img/Virginia_Beach-2019_09_01-3226.jpg" />
-	<Image src="img/Virginia_Beach-2019_09_01-3232.jpg" />
-	<Image src="img/Virginia_Beach-2019_09_01-3249.jpg" />
-	<Image src="img/Virginia_Beach-2019_09_01-3250.jpg" />
+	<div class="row">
+		<div class="col-6">
+			<Image src="img/Virginia_Beach-2019_09_01-3226.jpg" />
+			<Image src="img/Virginia_Beach-2019_09_01-3249.jpg" />
+		</div>
+		<div class="col-6">
+			<Image src="img/Virginia_Beach-2019_09_01-3232.jpg" />
+			<Image src="img/Virginia_Beach-2019_09_01-3250.jpg" />
+		</div>
+	</div>
 	<Image src="img/Virginia_Beach-2019_09_01-3253.jpg" />
 	<Image src="img/Virginia_Beach-2019_09_01-3254.jpg" />
 	<Image src="img/Virginia_Beach-2019_09_01-3261.jpg" />
